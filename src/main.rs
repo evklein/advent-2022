aa30b84e111e56ea93c780c997a14b4b637c49e4
Evan Klein <Evan.Klein@Milliman.com>
2022-12-01 09:50:47 -0500

    Adding command line functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let code = &args[1];
-    let input: &String = &args[2];
+    let (method, input_path) = parse_arguments(&args);
+    let input = fs::read_to_string(input_path).expect("File could not be read");
 
-    println!("Running {code} with input {input}");
+    match input.as_str() {
+
+    };
+}
+
+fn parse_arguments(args: &[String]) -> (String, String) {
+    let method: String = args[1].clone();
+    let input_path: String = args[2].clone();
+
+    (method, input_path)
 }
