56ddc38afbf63b5dd81c2a624cdf053ed9be7fa5
Evan Klein <Evan.Klein@Milliman.com>
2022-12-01 10:00:55 -0500

    Finished command line input implementation

---
+++ src/days/day1.rs
@@ -0,0 +1,3 @@
+pub fn part1(input: String) {
+    println!("Day 1 - Part 1 Initiated");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
 use std::fs;
 
+pub mod days;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let (method, input_path) = parse_arguments(&args);
     let input = fs::read_to_string(input_path).expect("File could not be read");
 
-    match input.as_str() {
-
+    match method.as_str() {
+        "d1" => days::day1::part1(input),
+        _ => println!("Invalid input."),
     };
 }
 
