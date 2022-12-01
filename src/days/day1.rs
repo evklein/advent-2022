ff1b98b7f23266aa306c7f21464fa44ac0dd9584
Evan Klein <Evan.Klein@Milliman.com>
2022-12-01 10:10:54 -0500

    Day1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,3 +1,21 @@
+use std::thread::current;
+
 pub fn part1(input: String) {
-    println!("Day 1 - Part 1 Initiated");
+    println!("Running Day 1 - Part 1");
+
+    let mut max_calories: i32 = 0;
+    let mut current_elf_calories: i32 = 0;
+
+    for line in input.lines() {
+        if (line.is_empty()) {
+            if (current_elf_calories > max_calories) {
+                max_calories = current_elf_calories;
+            }
+            current_elf_calories = 0;
+        } else {
+            current_elf_calories += line.parse::<i32>().unwrap();
+        }
+    }
+
+    println!("{max_calories}");
 }
