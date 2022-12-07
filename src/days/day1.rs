619b6bc604d25f4f0fa9a38d6f40d05801b646f8
Evan Klein <Evan.Klein@Milliman.com>
2022-12-07 13:14:04 -0500

    Cleaning up d1p2

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,6 +1,4 @@
 pub fn part1(input: String) {
-    println!("Running Day 1 - Part 1");
-
     let mut max_calories: i32 = 0;
     let mut current_elf_calories: i32 = 0;
 
@@ -19,41 +17,34 @@ pub fn part1(input: String) {
 }
 
 pub fn part2(input: String) {
-    println!("Running Day 1 - Part 2");
-
     let (mut first, mut second, mut third) = (0, 0, 0);
     let mut current_elf_calories: i32 = 0;
 
     for line in input.lines() {
         if line.is_empty() {
-            if current_elf_calories > first {
-                third = second;
-                second = first;
-                first = current_elf_calories;
-            } else if current_elf_calories > second {
-                third = second;
-                second = current_elf_calories;
-            } else if current_elf_calories > third {
-                third = current_elf_calories;
-            }
-            println!("Elf has {}", current_elf_calories);
-            println!("{} {} {}", first, second, third);
+            (first, second, third) = calculate_elf_calories(current_elf_calories, first, second, third);
             current_elf_calories = 0;
         } else {
             current_elf_calories += line.parse::<i32>().unwrap();
         }
     }
 
-    if current_elf_calories > first {
-        third = second;
-        second = first;
-        first = current_elf_calories;
-    } else if current_elf_calories > second {
-        third = second;
-        second = current_elf_calories;
-    } else if current_elf_calories > third {
-        third = current_elf_calories;
-    }
+    (first, second, third) = calculate_elf_calories(current_elf_calories, first, second, third);
 
     println!("{}", first + second + third);
+}
+
+fn calculate_elf_calories(current: i32, mut first: i32, mut second: i32, mut third: i32) -> (i32, i32, i32) {
+    if current > first {
+        third = second;
+        second = first;
+        first = current;
+    } else if current > second {
+        third = second;
+        second = current;
+    } else if current > third {
+        third = current;
+    }
+
+    (first, second, third)
 }
