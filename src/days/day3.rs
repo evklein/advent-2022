a3140da643b3d4b896553771208c7f94f807be7f
Evan Klein <Evan.Klein@Milliman.com>
2022-12-08 12:11:20 -0500

    D3p1

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,5 +1,24 @@
 pub fn part1(input: String) {
+    let mut priority_sum: i32 = 0;
+    let mut duplicates: Vec<char> = vec![];
 
+    input.lines()
+        .map(|line| line.split_at(line.len() / 2))
+        .for_each(|(first, second)| {
+            for c in first.chars() {
+                if second.contains(c) {
+                    duplicates.push(c);
+                    break;
+                }
+            }
+        });
+
+    priority_sum = duplicates.iter()
+                             .map(|c| {
+                                if c.is_lowercase() { (c.to_owned() as i32) - 96 }
+                                else { (c.to_owned() as i32) - 38 }
+                             }).sum();
+    println!("{:?}", priority_sum);
 }
 
 pub fn part2(input: String) {
